b7971f427cf95ba0f0696f4da7a0d76f30aaf5c0
lara123-pg <lamorimalvescavalcante@gmail.com>
2025-09-26 20:59:22 -0300

    chore: setup project environment

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Nunito, Lexend } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+  weight: ["700"]
+});
+
+const lexend = Lexend({
+  variable: "--font-lexend",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"]
+});
+
+export const metadata: Metadata = {
+  title: "FocusPlan",
+  description: "Sistema de planejamento de tarefas e estudo com Pomodoro",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${lexend.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
