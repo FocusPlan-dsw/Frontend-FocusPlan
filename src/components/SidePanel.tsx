1acb6f4945122f3b18a7458b3a81177b77cbf5ed
lara123-pg <lamorimalvescavalcante@gmail.com>
2025-09-28 21:20:35 -0300

    feat: add side panel

---
+++ src/components/SidePanel.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export function SidePanel() {
+    return (
+        <div className="bg-gradient w-full xl:max-w-[36rem] h-screen flex items-center justify-center max-xl:max-h-[10vh]">
+            <Image src="/white-logo.svg" alt="logo" width={432} height={109} className="max-xl:w-[10rem]" />
+        </div>
+    )
+}
