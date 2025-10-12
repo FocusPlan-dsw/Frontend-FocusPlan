975ac02febbcd28b2166648aa831b312f263ee9a
lara123-pg <lamorimalvescavalcante@gmail.com>
2025-10-11 23:53:13 -0300

    refactor: remove logout in step type

--- src/types/Step.ts
+++ src/types/Step.ts
@@ -1,1 +1,1 @@
-export type Step = "today" | "tomorrow" | "week" | "all" | "completed" | "overdue" | "reports" | "logout";
+export type Step = "today" | "tomorrow" | "week" | "all" | "completed" | "overdue" | "reports";
