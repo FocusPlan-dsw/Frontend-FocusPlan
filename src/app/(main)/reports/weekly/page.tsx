9165842085a166895378bcd6275e385896d83b50
evellyn489 <evellynrocha489@gmail.com>
2025-10-14 18:09:53 -0300

    feat: create the weekly report page

--- src/app/(main)/reports/weekly/page.tsx
+++ src/app/(main)/reports/weekly/page.tsx
@@ -16,13 +16,16 @@ export default function WeeklyReportsPage() {
   return (
     <div>
     <BackButton />
-      <div className="flex flex-col gap-20">
+      <div className="flex flex-col gap-10">
           <h1 className="text-3xl text-primary">Relatório Semanal</h1>
-          <main className="grid grid-cols-2 gap-10 w-full max-w-[1200px] mx-auto">
-              <InformationBlockReports quantity="20h e 30min" value="Tempo total percorrido" icon={Clock10}/>
-              <InformationBlockReports quantity="20" value="Tarefas Planejadas"/>
-              <InformationBlockReports quantity="10" value="Tarefas Concluídas"/>
-              <InformationBlockReports quantity="10" value="Tarefas Não Concluídas"/>
+          <main className="flex flex-col gap-10">
+            <p>Nessa semana (de 12 a 18 de outubro) o seu planejamento e horas líquidas de estudo foram coletados e você obteve os seguintes resultados:</p>
+            <div className="grid grid-cols-2 gap-10 w-full max-w-[1200px] mx-auto">
+                <InformationBlockReports quantity="20h e 30min" value="Tempo total percorrido" icon={Clock10}/>
+                <InformationBlockReports quantity="20" value="Tarefas Planejadas"/>
+                <InformationBlockReports quantity="10" value="Tarefas Concluídas"/>
+                <InformationBlockReports quantity="10" value="Tarefas Não Concluídas"/>
+            </div>
           </main>
       </div>
       
