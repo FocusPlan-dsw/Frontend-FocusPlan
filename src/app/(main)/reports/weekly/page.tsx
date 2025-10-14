372416e8ddbe96dd5e36ebfb8e06f808cd2be11d
evellyn489 <evellynrocha489@gmail.com>
2025-10-14 19:32:45 -0300

    feat: add total estimated time and percentage of tasks completed to weekly reports

--- src/app/(main)/reports/weekly/page.tsx
+++ src/app/(main)/reports/weekly/page.tsx
@@ -43,8 +43,10 @@ export default function WeeklyReportsPage() {
           <h1 className="text-3xl text-primary">Relatório Semanal</h1>
           <main className="flex flex-col gap-10">
             <p>Nessa semana (de 12 a 18 de outubro) o seu planejamento e horas líquidas de estudo foram coletados e você obteve os seguintes resultados:</p>
-            <div className="grid grid-cols-2 gap-10 w-full max-w-[1200px] mx-auto">
+            <div className="grid grid-cols-2 gap-10 w-full max-w-[1200px] mx-auto mb-15">
+              <InformationBlockReports quantity={formatSeconds(report.totalEstimatedTime)} value="Tempo total estimado" icon={Clock10}/>
                 <InformationBlockReports quantity={formatSeconds(report.totalTimeDedicated)} value="Tempo total percorrido" icon={Clock10}/>
+                <InformationBlockReports quantity={`${report.progressTasks} %`} value="Tarefas Concluídas"/>
                 <InformationBlockReports quantity={report.totalTasks} value="Tarefas Planejadas"/>
                 <InformationBlockReports quantity={report.completedTasks} value="Tarefas Concluídas"/>
                 <InformationBlockReports quantity={report.overdueTasks} value="Tarefas Não Concluídas"/>
