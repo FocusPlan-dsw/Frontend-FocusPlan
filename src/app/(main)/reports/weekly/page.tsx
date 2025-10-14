4d79d28d43e526958855eb68c2466dd438bf0107
evellyn489 <evellynrocha489@gmail.com>
2025-10-14 01:34:49 -0300

    feat: create report screen information block

--- src/app/(main)/reports/weekly/page.tsx
+++ src/app/(main)/reports/weekly/page.tsx
@@ -2,8 +2,9 @@
 
 import { useStep } from "@/context/StepContext";
 import { useEffect } from "react";
-import Link from 'next/link'; 
 import { BackButton } from "@/components/BackButton";
+import { InformationBlockReports } from "@/components/InformationBlockReports";
+import { Clock10 } from "lucide-react";
 
 export default function WeeklyReportsPage() {
   const { setStep } = useStep();
@@ -15,7 +16,15 @@ export default function WeeklyReportsPage() {
   return (
     <div>
     <BackButton />
-      <h1 className="text-3xl text-primary">Relatório Semanal</h1>
+      <div className="flex flex-col gap-20">
+          <h1 className="text-3xl text-primary">Relatório Semanal</h1>
+          <main className="grid grid-cols-2 gap-10 w-full max-w-[1200px] mx-auto">
+              <InformationBlockReports quantity="20h e 30min" value="Tempo total percorrido" icon={Clock10}/>
+              <InformationBlockReports quantity="20" value="Tarefas Planejadas"/>
+              <InformationBlockReports quantity="10" value="Tarefas Concluídas"/>
+              <InformationBlockReports quantity="10" value="Tarefas Não Concluídas"/>
+          </main>
+      </div>
       
       
     </div>
---
+++ src/components/InformationBlockReports.tsx
@@ -0,0 +1,21 @@
+type InformationBlockReportsProps = {
+        icon?: React.ElementType
+        quantity: string
+        value: string
+    }
+
+export function InformationBlockReports({ icon: Icon, quantity, value }: InformationBlockReportsProps) {
+    return (
+        <div className="flex flex-col items-center justify-center gap-4 w-full max-w-[339px] min-h-[110px] bg-light-gray border border-gray02 rounded-[10px] p-4 sm:p-5">
+            <div className="flex items-center gap-3">
+                {Icon && <Icon className="w-[30px] h-[30px] text-primary" />}
+                <p className="text-dark-gray text-3xl font-normal text-primary">
+                    {quantity}
+                </p>
+            </div>
+            <p className="text-dark-gray text-xl font-normal">
+                {value}
+            </p>
+        </div>
+    )
+}
