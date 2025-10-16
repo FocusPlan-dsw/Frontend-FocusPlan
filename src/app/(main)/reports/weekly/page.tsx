f270a981d096e1d9e2aa16eb90ed3581d06994ec
evellyn489 <evellynrocha489@gmail.com>
2025-10-16 00:48:51 -0300

    feat: integrates with the backend the select that filters the week

--- src/app/(main)/reports/weekly/page.tsx
+++ src/app/(main)/reports/weekly/page.tsx
@@ -14,6 +14,8 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 export default function WeeklyReportsPage() {
   const { setStep } = useStep();
 
+  const [selectedWeek, setSelectedWeek] = useState("thisWeek");
+
   const [report, setReport] = useState({
     "totalTasks": 0,
     "completedTasks": 0,
@@ -26,16 +28,24 @@ export default function WeeklyReportsPage() {
       currentWeek: {
         start: null,
         end: null
+      },
+      previousWeek: {
+        start: null,
+        end: null
       }
     }
   })
+
+  const displayedPeriod = selectedWeek === 'thisWeek'
+  ? report.period?.currentWeek
+  : report.period?.previousWeek;
 
   useEffect(() => {
     setStep("reports");
 
     async function fetchReport() {
       try {
-        const response = await api.get("/reports/weekly");
+        const response = await api.get(`/reports/weekly?period=${selectedWeek}`);
         setReport(response.data);
       } catch (error) {
         console.error("Erro ao buscar o relatório semanal:", error);
@@ -43,7 +53,7 @@ export default function WeeklyReportsPage() {
     }
 
     fetchReport();
-  }, [setStep]);
+  }, [setStep, selectedWeek]);
 
   return (
     <div>
@@ -51,7 +61,10 @@ export default function WeeklyReportsPage() {
       <div className="flex flex-col gap-10">
           <h1 className="text-3xl text-primary">Relatório Semanal</h1>
           <main className="flex flex-col gap-10">
-            <Select>
+            <Select
+            value={selectedWeek}
+            onValueChange={(value) => setSelectedWeek(value)}
+            >
               <SelectTrigger label="Semana" className="w-[200px]">
                 <SelectValue placeholder="Selecione a semana" />
               </SelectTrigger>
@@ -60,7 +73,7 @@ export default function WeeklyReportsPage() {
                 <SelectItem value="previousWeek">Semana passada</SelectItem>
               </SelectContent>
             </Select>
-            <p>Nessa semana de {formatDate(report.period?.currentWeek?.start)} a {formatDate(report.period?.currentWeek?.end)} o seu planejamento e horas líquidas de estudo foram coletados e você obteve os seguintes resultados:</p>
+            <p>Na semana de {formatDate(displayedPeriod?.start)} a {formatDate(displayedPeriod?.end)} o seu planejamento e horas líquidas de estudo foram coletados e você obteve os seguintes resultados:</p>
             <div className="flex flex-wrap gap-x-5 gap-y-10 w-full max-w-[1200px] mx-auto mb-15 items-start">
               <InformationBlockReports quantity={minutesToHHMM(report.totalEstimatedTime)} value="Tempo total estimado" icon={Clock10}/>
                 <InformationBlockReports quantity={formatSeconds(report.totalTimeDedicated)} value="Tempo total percorrido" icon={Clock10}/>
