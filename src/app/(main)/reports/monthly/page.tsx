72031a85c8342848010903f876f118d0ec64198b
evellyn489 <evellynrocha489@gmail.com>
2025-10-16 01:00:28 -0300

    feat: create monthly report page

---
+++ src/app/(main)/reports/monthly/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { useStep } from "@/context/StepContext";
+import { useEffect, useState } from "react";
+import { BackButton } from "@/components/BackButton";
+import { InformationBlockReports } from "@/components/InformationBlockReports";
+import { Clock10 } from "lucide-react";
+import { formatSeconds } from "@/utils/FormatSeconds";
+import { minutesToHHMM } from "@/utils/ConvertMinutesToHHMM";
+import { formatDate } from "@/utils/FormatDate";
+import api from "@/lib/api";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+export default function WeeklyReportsPage() {
+  const { setStep } = useStep();
+
+  const [report, setReport] = useState({
+    "totalTasks": 0,
+    "completedTasks": 0,
+    "overdueTasks": 0,
+    "progressTasks": 0,
+    "percentageCompleted": 0,
+    "totalEstimatedTime": "",
+    "totalTimeDedicated": 0,
+    period: {
+      currentWeek: {
+        start: null,
+        end: null
+      },
+      previousWeek: {
+        start: null,
+        end: null
+      }
+    }
+  })
+
+
+  useEffect(() => {
+    setStep("reports");
+
+    async function fetchReport() {
+      try {
+        const response = await api.get(`/reports/weekly?`);
+        setReport(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar o relatório semanal:", error);
+      }
+    }
+
+    fetchReport();
+  }, [setStep]);
+
+  return (
+    <div>
+    <BackButton />
+      <div className="flex flex-col gap-10">
+          <h1 className="text-3xl text-primary">Relatório Mensal</h1>
+          <main className="flex flex-col gap-10">
+            <Select
+            //value={selectedWeek}
+            //onValueChange={(value) => setSelectedWeek(value)}
+            >
+              <SelectTrigger label="Mês" className="w-[200px]">
+                <SelectValue placeholder="Selecione um mês para visualizar" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="thisWeek">Janeiro</SelectItem>
+                <SelectItem value="previousWeek">Fevereiro</SelectItem>
+                <SelectItem value="previousWeek">Março</SelectItem>
+                <SelectItem value="previousWeek">Abril</SelectItem>
+                <SelectItem value="previousWeek">Maio</SelectItem>
+                <SelectItem value="previousWeek">Junho</SelectItem>
+                <SelectItem value="previousWeek">Julho</SelectItem>
+                <SelectItem value="previousWeek">Agosto</SelectItem>
+                <SelectItem value="previousWeek">Setembro</SelectItem>
+                <SelectItem value="previousWeek">Outubro</SelectItem>
+                <SelectItem value="previousWeek">Novembro</SelectItem>
+                <SelectItem value="previousWeek">Dezembro</SelectItem>
+              </SelectContent>
+            </Select>
+            <p>No mês de dessse ano o seu planejamento e horas líquidas de estudo foram coletados e você obteve os seguintes resultados:</p>
+            <div className="flex flex-wrap gap-x-5 gap-y-10 w-full max-w-[1200px] mx-auto mb-15 items-start">
+              <InformationBlockReports quantity={minutesToHHMM(report.totalEstimatedTime)} value="Tempo total estimado" icon={Clock10}/>
+                <InformationBlockReports quantity={formatSeconds(report.totalTimeDedicated)} value="Tempo total percorrido" icon={Clock10}/>
+                <InformationBlockReports quantity={`${report.progressTasks}%`} value="Tarefas Concluídas"/>
+                <InformationBlockReports quantity={report.totalTasks} value="Tarefas Planejadas"/>
+                <InformationBlockReports quantity={report.completedTasks} value="Tarefas Concluídas"/>
+                <InformationBlockReports quantity={report.overdueTasks} value="Tarefas Não Concluídas"/>
+            </div>
+          </main>
+      </div>
+      
+      
+    </div>
+  );
+}
