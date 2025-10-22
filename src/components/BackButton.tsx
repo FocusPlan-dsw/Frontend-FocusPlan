dab501e8a937a1dddf4abf77c4ca5a4e8d1a42f7
lara123-pg <lamorimalvescavalcante@gmail.com>
2025-10-22 02:41:15 -0300

    feat:add back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -3,13 +3,23 @@
 import { CircleArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 
-export function BackButton() {
+interface BackButtonProps {
+    onBack?: () => void
+}
+
+export function BackButton({ onBack }: BackButtonProps) {
     const router = useRouter();
     
     return (
         <button 
             className="flex items-center gap-2 mb-10 text-dark-gray cursor-pointer hover:underline px-2 py-1 -ml-2"
-            onClick={() => router.back()}
+            onClick={() => {
+                if (onBack) {
+                    onBack();
+                } else {
+                    router.back();
+                }
+            }}
             aria-label="Voltar para página anterior"
         >
             <CircleArrowLeft className="w-5 h-5" />
